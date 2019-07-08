9e0c7801a6fd6910c896c2bb7f5c44a5248f008b
v4rden <denys.variah@gmail.com>
2019-07-08 18:49:25 +0300

    Initial commit

---
+++ ThreadingFormsDemo/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ThreadingFormsDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timeLable = new System.Windows.Forms.Label();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timeLable
+            // 
+            this.timeLable.AutoSize = true;
+            this.timeLable.Location = new System.Drawing.Point(358, 135);
+            this.timeLable.Name = "timeLable";
+            this.timeLable.Size = new System.Drawing.Size(55, 17);
+            this.timeLable.TabIndex = 0;
+            this.timeLable.Text = "Waiting";
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(361, 264);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 1;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.timeLable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLable;
+        private System.Windows.Forms.Button updateButton;
+    }
+}
+
